05264e039e3a43ef64d065c513860b8677708644
Alex <alexander.thomas@student.onslow.school.nz>
2021-03-11 12:07:13 +1300

    First bit

---
+++ Draw.java
@@ -0,0 +1,44 @@
+import java.random;
+/**
+ * Draws 8 unique numbers (lotto balls) and sticks them into an array
+ * Numbers between 1 and 40
+ *  
+ * @author AT + LCH
+ * @version 11/3/21
+ */
+public class Draw
+{
+    // instance variables 
+    private final int DRAWLENGTH = 8;
+    private final int MAXDRAW = 40;
+    private final int MINDRAW = 1;
+    
+    // array
+    public int[] draw = new int[DRAWLENGTH]; 
+    
+
+    /**
+     * Constructor for objects of class Draw
+     */
+    public Draw()
+    {
+        // initialise instance variables
+        draw(); 
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int draw()
+    {
+        // put your code here
+        for (int num = 0; num < lotto.length; num++)
+        {
+            int ball = int Math.random() * 40;
+            for i   
+        }
+    }
+}
